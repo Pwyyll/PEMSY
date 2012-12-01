423d7d194cd755a4df17abadbcf612536287bf11
kienluen <kienluen.to@gmx.de>
2012-12-01 11:44:42 +0100

    - CHANGED boolean to uint8_t - ADDED prototype lcd_locate(uint8_t row, uint8_t col)

--- lcd.h
+++ lcd.h
@@ -1,14 +1,17 @@
 #if !defined __LCD_H
 #define __LCD_H
-#include <stdbool.h>
+
+#include <avr/io.h>
+#include <stdio.h>
+
 static volatile uint8_t *const lcd_port[4];
 static const uint8_t lcd_pin[4];
 static volatile uint8_t *const lcd_reg[4];
 volatile const uint8_t wait_long;
 volatile const uint8_t wait_short;
 //zustand: false fuer lesen, true fuer schreiben
-bool zustand;
-bool init_mode; // flag for Init des CR mit EINEM Nibble
+uint8_t zustand;
+uint8_t init_mode; // flag for Init des CR mit EINEM Nibble
 volatile const uint8_t pwm_standard;
 
 void lcd_init(void);
@@ -20,5 +23,9 @@ void lcd_init_write(void);
 void lcd_write_data(char data);
 
 void lcd_write_cmd(char cmd);
+
+int lcd_put(char c, FILE *p);
+
+void lcd_locate(uint8_t row, uint8_t col);
 
 #endif
