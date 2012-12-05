16014b766d68c1970fc9796925d53aa4ad7f77b8
Pwyyll <philipp-wojaczek@gmx.net>
2012-12-05 12:06:02 +0100

    Stand 05.12.12: Interrupt fuer Tastatur und Taster geschrieben

--- programm03.c
+++ programm03.c
@@ -12,13 +12,30 @@ void init(void)
 	timer_init();
 	pwm_init();
 	lcd_init();
-		
 	return;
 }
-
+void interrupt_init()
+{
+	//PD3/INT1 als Interrupt fuer Tastatur
+	// mit Clock der Tastatur verbinden
+	MCUCR |= (1<<ISC11); //fallende Flanke
+	MCUCR &= ~(1<<ISC10);
+	GICR |= (1<<INT1);
+	DDRD &= ~(1<<PD3); // Eingang
+	PORTD |= (1<<PD3); // Pull-up Widerstand auf high
+	
+	//PD2/INT0 als Interrupt fuer Taster
+	MCUCR |= (1<<ISC01); // fallende Flanke
+	MCUCR &= ~(1<<OSC00);
+	GICR |= (1<<INT0);
+	DDRD &= ~(1<<PD2); // Eingang
+	PORTD |= (1<<PD2); // Pull-up Widerstand auf high
+	return;
+}
 int main(void)
 {
 	init();
+	interrupt_init();
 	sei(); //INTERRUPTS EINSCHALTEN NICHT VERGESSEN!!!!!!!!!!!!!!!!!!!!
 	//uint8_t contrast = 0;
 	//uint32_t wartezeit = 1000;
