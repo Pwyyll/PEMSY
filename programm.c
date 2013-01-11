c9a79f853cb5f8a640228e95a46ca50174758ef8
Pwyyll <philipp-wojaczek@gmx.net>
2013-01-11 13:40:45 +0100

    Neue Version. 11.01.13

--- programm.c
+++ programm.c
@@ -1,33 +1,74 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 #include "timer.h"
 #include "led.h"
 #include "pwm.h"
-//#include "lcd.h"
+#include "lcd.h"
+#include "tastatur.h"
+#include "usart.h"
+
+
+volatile uint16_t baud = 9600;
+
 
 void init(void)
 {
 	led_init();
 	timer_init();
 	pwm_init();
+	lcd_init();
+	tastatur_init();
+	usart_init(baud);
 	return;
+}
+
+void interrupt_init(void)
+{
+	cli();
+	//PD3/INT1 als Interrupt fuer Tastatur
+	// mit Clock der Tastatur verbinden
+	MCUCR |= (1<<ISC11); //fallende Flanke
+	MCUCR &= ~(1<<ISC10);
+	GICR |= (1<<INT1);
+	DDRD &= ~(1<<PD3); // Eingang
+	PORTD |= (1<<PD3); // Pull-up Widerstand auf high
+
+	//PD2/INT0 als Interrupt fuer Taster
+	MCUCR |= (1<<ISC01); // fallende Flanke
+	MCUCR &= ~(1<<ISC00);
+	GICR |= (1<<INT0);
+	DDRD &= ~(1<<PD2); // Eingang
+	PORTD |= (1<<PD2); // Pull-up Widerstand auf high
+
+	sei();
+	return;
+}
+
+//Interruptroutine des Tasters
+ISR(SIG_INTERRUPT0)
+{
+	led_toggle(0);
 }
 
 int main(void)
 {
+
 	init();
-	sei(); //NICHT INTERRUPTS EINSCHALTEN VERGESSEN!!!!!!!!!!!!!!!!!!!!
-	uint8_t contrast = 0;
-	uint32_t wartezeit = 1000;
-	while(1)
-	{
-		set_ocr(contrast);
-		led_on(3);
-		contrast++;
-//		led_toggle(1);
-		timer_wait(wartezeit);
-		led_off(3);
-		timer_wait(wartezeit);
-		led_toggle(0);
-	}
+	interrupt_init();
+	sei(); //INTERRUPTS EINSCHALTEN NICHT VERGESSEN!!!!!
+	lcd_locate(0,0);
+
+	unsigned char datenwort = 'A';
+
+while(1)
+{
+//	char keyb_data[33];
+
+//	fgets(keyb_data, 33, fdevopen(NULL, keyb_get));
+
+	send_data(datenwort);
+
+}
+
 }
--- usart.c
+++ usart.c
@@ -1,29 +1,76 @@
 #include <avr/io.h>
 
-volatile uint8_t rx_enabled = 0; //flag: 0 fuer empfangen, 1 fuer senden (vgl. uint8_t zustand in lcd.c)
+volatile uint8_t empfang_usart = 0;
+
+volatile uint8_t rx_enabled_usart = 0; //flag: 0 fuer empfangen, 1 fuer senden (vgl. uint8_t zustand in lcd.c)
+
 //Initialisierung des USART (allgemein)
-void usart_init()
+void usart_init(uint16_t baud)
 {
-//Interrupts enablen: RX, TX, UDR
-UCSRB = (1<<RXCIE) | (1<<TXCIE) (1<<UDRIE);
-//URSEL = 1, um UCSRC beschreiben zu können
-UCSRC |= (1<<URSEL);
-//UMSEL = 0: asynchron
-UCSRC &= ~(1<<UMSEL);
-//UPM1:0 = 0 0 : keine Paritaet
-UCSRC &= ~((1<<UPM1) | (1<<UPM0));
-//USBS = 0: kein Stopbit
-UCSRC &= ~(1<<USBS);
-//UCSZ2:0 = 0 0 0: 8-Bit Datenmodus
-UCSRB = ~(1<<UCSZ2);
-UCSRC = ((1<<UCSZ1) | (UCSZ0));
-//URSEL = 0: zum Beschreiben von UBRR
-UCSRC &= ~(1<<URSEL);
-UBRR = 25; //Baudrate von 9600 bei f_Clock = 4MHz, U2X = 0 (kein double-speed)
+	DDRD |= (1<<PD1);	//AUsgang
+	PORTD &= ~(1<<PD1);
+
+	DDRD &= ~(1<<PD0);	//EIngang
+	PORTD |= (1<<PD0);
+
+
+	UBRRH &= ~(1<<URSEL);	//URSEL = 0: zum Beschreiben von UBRR
+	UBRRH = (0x00>>8);
+	UBRRL = 103;	//Baudrate von 9600 bei f_Clock = 4MHz, U2X = 0 (kein double-speed)
+	UCSRB = (1<<RXEN) | (1<<TXEN);
+	//UCSRC &= ~((1<<UPM1) | (1<<UPM0));	//UPM1:0 = 0 0 : keine Paritaet
+	UBRRH |= (1<<URSEL);	//URSEL = 1, um UCSRC beschreiben zu können
+	UCSRC |= (1<<UCSZ0);	// 8-Bit Datenmodus
+	UCSRC |= (1<<UCSZ1);
+	//UCSRC &= ~(1<<USBS);	//USBS = 0: kein Stopbit
+	//UCSRC &= ~(1<<UMSEL);	//UMSEL = 0: asynchron
+	UCSRC |= (1<<URSEL);	
+
+	//Interrupts enablen: RX, TX, UDR
+//	UCSRB = (1<<RXCIE) | (1<<TXCIE) | (1<<UDRIE);
+	UCSRB = (1<<TXCIE);
+	
+
+}
+
+void send_data(unsigned char datenwort)
+{
+//	unsigned char data = 'A';
+	// nur Daten ins Register schreiben, falls UDR flag gesetzt ist!
+	while (!(UCSRA & (1<<UDRE)))
+	{
+		;	//warteschleife, bis Flag gesetzt wurde
+	}
+	UDR = 'A';
+//	printf("GESENDET");
+//	lcd_clear();
+}
+
+ISR(SIG_USART_RECV)
+{
+	empfang_usart = 1;
+}
+
+char usart_receive()
+{
+	if (empfang_usart ==1)
+	{
+		char tempwort = 0;
+		if (tempwort != '\r')
+		{
+			return tempwort;
+		} else if (tempwort == '\r')
+		{
+			return ('\n');
+		}
+	}
+	return;
+}
+
 /*
 Muss fuer neues Senden TXEN wieder gesetzt werden, falls zwischendurch Daten empfangen wurden?
 Also so wie bei der lcd.c?
-Oder muss man alles einmal enablen, und dann werden Daten empfangen/gesendet, je nachdem, 
+Oder muss man alles einmal enablen, und dann werden Daten empfangen/gesendet, je nachdem,
 was im Datenregister steht?
 falls immer wieder neu konfiguriert werden muss: zwei neue Funktionen:
 
@@ -40,39 +87,3 @@ rx_enabled = 0;
 }
 
 */
-//Aufruf zum enablen des Empfangens
-UCSRB |= (1<<TXEN);
-//Aufruf zum enablen des Sendens
-UCSRB |= (1<<RXEN);
-
-}
-
-void send_data(uint8_t datenwort)
-{
-// nur Daten ins Register schreiben, falls UDR flag gesetzt ist!
-while ((UCSRA & UDRE) != 1)
-{
-//warteschleife, bis Flag gesetzt wurde
-}
-UDR = datenwort;
-}
-
-ISR(SIG_USART_RECV)
-{
-	empfang = 1;
-}
-
-char usart_receive() 
-{
-	if (empfang ==1)
-	{
-		char tempwort = 0;
-		if (tempwort != '\r'
-		{
-			return tempwort;
-		} else if (tempwort == '\r')
-			return ('\n');
-		}
-	}
-	return;
-}
